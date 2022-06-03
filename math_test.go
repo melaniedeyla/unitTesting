444f95e4b7791057a0881f31e9052dc00ef4bb34
melaniedeyla <72774998+melaniedeyla@users.noreply.github.com>
2022-06-03 15:36:10 +1000

    Update math_test.go

--- math_test.go
+++ math_test.go
@@ -11,3 +11,33 @@ func TestAdd(t *testing.T) {
 		t.Logf("Add(1, 3) PASSED. EXPECTED %d, got %d\n", 4, result)
 	}
 }
+
+func TestSubtract(t *testing.T) {
+	result := Subtract(5, 3)
+
+	if result != 2 {
+		t.Errorf("Subtract(5, 3) FAILED. Expected %d, got %d\n", 2, result)
+	} else {
+		t.Logf("Subtract(5, 3) PASSED. Expected %d, got %d\n", 2, result)
+	}
+}
+
+func TestDivide(t *testing.T) {
+	result := Divide(9, 3)
+
+	if result != 3 {
+		t.Errorf("Divide(9, 3) FAILED. Expected %d, got %f", 3, result)
+	} else {
+		t.Logf("Divide(9, 3) PASSED. Expected %d, got %f", 3, result)
+	}
+}
+
+func TestMultiply(t *testing.T) {
+	result := Multiply(2, 4)
+
+	if result != 8 {
+		t.Errorf("Multiply(2, 4) FAILED. Expected %d, got %d", 8, result)
+	} else {
+		t.Logf("Multiply(2, 4) PASSED. Expected %d, got %d", 8, result)
+	}
+}
